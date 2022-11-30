89e1570137a584fb42ba62b40fa35f940ad6470d
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-11-30 09:18:15 +0000

    [Update]11. ActionView::MissingTemplate手前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top,:about]
 
   private
 
   def after_sign_in_path_for(resource)
-    root_path
+    books_path
   end
 
   def after_sign_out_path_for(resource)
