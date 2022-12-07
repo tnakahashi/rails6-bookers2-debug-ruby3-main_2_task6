0012c0a78be853d7849dfb6423f2a65ef9284528
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-12-07 08:41:45 +0000

    [Finish]コメント機能の非同期追加_応用課題6完了

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,7 +8,8 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
